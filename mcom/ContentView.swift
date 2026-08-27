8e1cba678c2af78f7850518b66dce8f1296b12b3
coldmoon <398612365@qq.com>
2026-08-27 14:32:54 +0800

    first commit

---
+++ mcom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mcom
+//
+//  Created by coldmoon on 2026/8/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mcom/mcomApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcomApp.swift
+//  mcom
+//
+//  Created by coldmoon on 2026/8/27.
+//
+
+import SwiftUI
+
+@main
+struct mcomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
